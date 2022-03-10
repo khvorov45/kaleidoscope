ec1742aeb85cd66ca58f3835f4098b21b2c1aeef
Arseniy Khvorov <khvorov45@gmail.com>
2022-03-10 14:51:23 +1100

    update linux build

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
-gcc -g -Wall code/kaleidoscope.c -o build/kaleidoscope.bin
+gcc -g -Wall code/kaleidoscope.c -o build/kaleidoscope.bin \
+	-I"/usr/include/llvm-c-11" -lLLVM-11 \
+	-Wno-switch -Wno-unused-function
